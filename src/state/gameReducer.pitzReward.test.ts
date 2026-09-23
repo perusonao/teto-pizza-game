25e527590e0061c48c2b2693f5fc548fe09f14c6
perusonao <58flyingcar@gmail.com>
2026-09-24 01:34:57 +0900

    test: align Dex seed with ScoreBreakdown v2

--- src/state/gameReducer.pitzReward.test.ts
+++ src/state/gameReducer.pitzReward.test.ts
@@ -9,10 +9,10 @@ import { walkPostBakeToResult } from "./testSupport/postBakeFlow";
 const MARGHERITA_DISCOVERED_DEX = registerScoreToDex(EMPTY_DEX, "margherita", {
   total: 80,
   stars: 4,
-  sauce: 20,
-  pieces: 20,
-  recipe: 20,
-  bake: 20,
+  matchScore: 80,
+  ingredientScore: 80,
+  placementScore: 80,
+  bakeScore: 80,
 }).dex;
 
 /**
