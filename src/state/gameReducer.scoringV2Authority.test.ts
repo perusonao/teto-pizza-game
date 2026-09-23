bb674aed174adfe346645f83a6cd17f12a1a5d60
perusonao <58flyingcar@gmail.com>
2026-09-24 01:35:01 +0900

    test: align Dex seed with ScoreBreakdown v2

--- src/state/gameReducer.scoringV2Authority.test.ts
+++ src/state/gameReducer.scoringV2Authority.test.ts
@@ -19,10 +19,10 @@ const MARGHERITA_DISCOVERED_DEX = registerScoreToDex(EMPTY_DEX, "margherita", {
   // its only purpose is making Margherita a legal discovered Mission candidate.
   total: 20,
   stars: 1,
-  sauce: 20,
-  pieces: 20,
-  recipe: 20,
-  bake: 20,
+  matchScore: 80,
+  ingredientScore: 80,
+  placementScore: 80,
+  bakeScore: 80,
 }).dex;
 
 /**
